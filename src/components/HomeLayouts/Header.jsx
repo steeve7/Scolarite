8dc58701d68766162b7c8591c6d1262c2596926a
MC_DESMOND <macdesmonddikeibeh@gmail.com>
2024-10-11 15:46:36 +0100

    hhhhj

--- src/components/HomeLayouts/Header.jsx
+++ src/components/HomeLayouts/Header.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import style from "./style.module.css"
 import { mergeText } from '@/app/add'
-import logo from "@/app/logo.png"
+import logo from "@/app/assets/logo.png"
+import menu from "@/app/assets/menu.png"
 
 function NavBar(props){
-    return <div className={mergeText("flex justify-between items-center w-full",style.navbar)}>
-    <div className={style.navitem}><img src={logo} className={style.navlogo} alt="logo unavaliable" /></div>
-    <div className={mergeText(style.navlinks,"flex gap-10",style.navitem)}>
-      <a  href ="" className={style.navlink}>Home</a>
-      <a  href ="" className={style.navlink}>Pricing/Plans</a>
-      <a  href ="" className={style.navlink}>About us</a>
-      <a  href ="" className={style.navlink}>Blog</a>
-      <a  href ="" className={style.navlink}>Become an Affiliate</a>
+  useEffect(function(){
+    console.log(logo)
+  },[])
+    return <div className={mergeText("flex justify-center items-center w-full",style.navbar)}>
+    <div className={mergeText(style.betweencenter,style.navinner)}>
+      <div className={mergeText(style.betweencenter,style.naviconwrapper,style.navitem)}>
+        <img src={logo}  className={style.navlogo} alt="logo unavaliable" />
+        <img src={menu}  className={style.navmenuicon} alt="logo unavaliable" />
+
+        </div>
+      <div className={mergeText(style.betweencenter,style.navres)}>
+        <div className={mergeText(style.navlinks,"flex",style.navitem)}>
+          <a  href ="" className={style.navlink}>Home</a>
+          <a  href ="" className={style.navlink}>Pricing/Plans</a>
+          <a  href ="/about" className={style.navlink}>About us</a>
+          <a  href ="" className={style.navlink}>Blog</a>
+          <a  href ="" className={style.navlink}>Become an Affiliate</a>
+        </div>
+        <div className={style.navitem}>
+          <div className={mergeText(" ",style.navauth)}>LOGIN</div>
+          <div className={mergeText("bg-white text-black ",style.navauth)}>SIGN UP</div>
+        </div>
+      </div>
     </div>
-    <div className={style.navitem}></div>
   </div>
 }
 
