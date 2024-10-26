1b96f8e0edbaa75ce3c248251f4037ca9131b5c7
ofoegbu stephen <stephenchidindu12@gmail.com>
2024-10-26 15:02:22 +0100

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
-import Image from 'next/image'
+import About from '@/components/Section/About/About';
+import Analytic from '@/components/Section/Analytic/Analytic';
+import Choose from '@/components/Section/Choose/Choose';
+import Faq from '@/components/Section/FAQ/Faq';
+import Hero from '@/components/Section/Hero/Hero';
+import Plan from '@/components/Section/Plan/Plan';
+import Testimonial from '@/components/Section/Testimonial/Testimonial';
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Scholarite</h1>
+      <Hero />
+      <About />
+      <Choose />
+      <Analytic />
+      <Plan />
+      <Testimonial />
+      <Faq />
     </div>
   );
 }
