1cca2aa279b6f7a4e6e997554cafc09326848aff
Ballylight <ballylight153@gmail.com>
2024-11-01 16:06:51 +0100

    ALL ERRORS FIXED

--- src/app/blogs/[id]/page.jsx
+++ src/app/blogs/[id]/page.jsx
@@ -1,26 +1,63 @@
+// // [id].js
+// "use client"
+// // import { useRouter } from 'next/navigation';
+// // import { useRouter } from 'next/router';
+
+// import styles from './style.module.css';
+// import { useParams } from 'next/navigation';
+// import { BlogPosts } from '../BlogPostsData';
+
+// function BlogPost() {
+//   /* const router = useRouter();
+//   const { id } = router.query; */
+//   const { id } = useParams();
+//   // You would typically fetch post details from an API or data file based on the `id`
+//   // For this example, we'll use a placeholder post
+//   const post = BlogPosts.find((postObj)=> postObj.id == Number(id))
+//   console.log(id)
+//   // const post = {
+//   //   title: "Sample Blog Post Title",
+//   //   date: "Sunday, 1 Jan 2023",
+//   //   content: "This is a sample blog post content. Replace this with actual blog content.",
+//   //   imgSrc: "/images/post1-image.jpg",
+//   // };
+
+//   return (
+//     <div className={styles.blogPost}>
+//       <img src={post.imgSrc} alt={post.title} className={styles.postImage} />
+//       <div className={styles.postintrowrapper}>
+//         <div className={styles.piwinner}>
+//           <h1 className={styles.postTitle}>{post.title}</h1>
+//           <p className={styles.postDate}>
+//             <div className={styles.pageauthor}>Written By {post.author}</div> 
+//             {post.date}
+//             </p>
+//         </div>
+//     </div>
+//       <div className={styles.postContent}>{post.content.map((paragraph,index)=><div key={`key-wrapper-${index}`} className={styles.contentwrapper}>
+//       <div key={`key-title-${index}`}className={styles.contenttitle}>{paragraph.title}</div>
+//       <div key={`key-content-${index}`}className={styles.contentcontent}>{paragraph.content}</div>
+//       </div>)}</div>
+//     </div>
+//   );
+// };
+
+// export default BlogPost; 
+
+
+
+
 // [id].js
-"use client"
-// import { useRouter } from 'next/navigation';
-// import { useRouter } from 'next/router';
+"use client";
 
 import styles from './style.module.css';
 import { useParams } from 'next/navigation';
 import { BlogPosts } from '../BlogPostsData';
 
 function BlogPost() {
-  /* const router = useRouter();
-  const { id } = router.query; */
   const { id } = useParams();
-  // You would typically fetch post details from an API or data file based on the `id`
-  // For this example, we'll use a placeholder post
-  const post = BlogPosts.find((postObj)=> postObj.id == Number(id))
-  console.log(id)
-  // const post = {
-  //   title: "Sample Blog Post Title",
-  //   date: "Sunday, 1 Jan 2023",
-  //   content: "This is a sample blog post content. Replace this with actual blog content.",
-  //   imgSrc: "/images/post1-image.jpg",
-  // };
+  const post = BlogPosts.find((postObj) => postObj.id == Number(id));
+  console.log(id);
 
   return (
     <div className={styles.blogPost}>
@@ -28,18 +65,22 @@ function BlogPost() {
       <div className={styles.postintrowrapper}>
         <div className={styles.piwinner}>
           <h1 className={styles.postTitle}>{post.title}</h1>
-          <p className={styles.postDate}>
-            <div className={styles.pageauthor}>Written By {post.author}</div> 
+          <div className={styles.postDate}>
+            <span className={styles.pageauthor}>Written By {post.author}</span> 
             {post.date}
-            </p>
+          </div>
         </div>
-    </div>
-      <div className={styles.postContent}>{post.content.map((paragraph,index)=><div key={`key-wrapper-${index}`} className={styles.contentwrapper}>
-      <div key={`key-title-${index}`}className={styles.contenttitle}>{paragraph.title}</div>
-      <div key={`key-content-${index}`}className={styles.contentcontent}>{paragraph.content}</div>
-      </div>)}</div>
+      </div>
+      <div className={styles.postContent}>
+        {post.content.map((paragraph, index) => (
+          <div key={`key-wrapper-${index}`} className={styles.contentwrapper}>
+            <div key={`key-title-${index}`} className={styles.contenttitle}>{paragraph.title}</div>
+            <div key={`key-content-${index}`} className={styles.contentcontent}>{paragraph.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-};
+}
 
-export default BlogPost; 
+export default BlogPost;
