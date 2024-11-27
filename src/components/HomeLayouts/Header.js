d7c1288021713e19c60ca60ad6eff4efd34eefe0
ofoegbu stephen <stephenchidindu12@gmail.com>
2024-11-27 10:25:08 +0100

    in progress

--- src/components/HomeLayouts/Header.js
+++ src/components/HomeLayouts/Header.js
@@ -1,18 +1,26 @@
-import NavbarMain from "@/components/HomeLayouts/navbar"
-import style from "./navbar.module.css"
-import NavBar from '@/components/HomeLayouts/navbar'
+"use client";
+import NavbarMain from "@/components/HomeLayouts/navbar";
+import style from "./navbar.module.css";
+import NavBar from "@/components/HomeLayouts/navbar";
+import { useEffect } from "react";
+import { mergeText } from "@/app/add";
 // import React, { useEffect } from 'react'
 
-
 function Header() {
+  var classes = [style.header];
+  useEffect(function () {
+    document.querySelectorAll("NONE").forEach((el) => {
+      el.style.display = "none";
+    });
+  }, []);
   return (
     <>
       <div id="HEADER_SECTION" className={mergeText(...classes)}>
-        <NavbarMain/>
+        <NavbarMain />
       </div>
-      <div style={{marginBottom:"130px"}}></div>
+      <div style={{ marginBottom: "130px" }}></div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
