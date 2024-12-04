349f97f98ba5ef2c58361d302efbf04ff70d4993
Chigozirim victor <chigozirimvictorasogwa@gmail.com>
2024-12-04 11:45:49 -0800

    Finished Commit

--- src/app/paymentPageforCustom/page.js
+++ src/app/paymentPageforCustom/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect,useRef } from 'react'
+import React, { useEffect } from 'react'
 import './custom.css'
 import Image from 'next/image'
 import { color } from 'framer-motion'
@@ -10,239 +10,274 @@ import { useState } from 'react'
 
 export default function paymentPageforCustomPlan() {
 
-    const dateInputRef = useRef(null)
-    const [cardNmuber,setCardNumber] = useState("")
-    const [cvv,setCvv] = useState("")
+   // const dateInputRef = useRef(null)
+   const [cardNmuber,setCardNumber] = useState("")
+   const [cvv,setCvv] = useState("")
 
 
 
-    // useEffect(()=>{
-    //         if(cardNmuber.length > 16) return
-    // },[])
+   // useEffect(()=>{
+   //         if(cardNmuber.length > 16) return
+   // },[])
 
-    useEffect(()=>{
-        let emailEl = document.getElementById("email")
-        emailEl.addEventListener(EventName,()=>{
-            if ((String(emailEl.value).toLowerCase().includes("@") && String(emailEl.value).toLowerCase().includes(".com"))== false){
-                console.log("success ")
-            }else{
-                console.log("<error effects here>")
-            }
-        })
-        
-    },[])
+   useEffect(()=>{
+       let emailEl = document.getElementById("email")
+       emailEl.addEventListener(EventName,()=>{
+           if ((String(emailEl.value).toLowerCase().includes("@") && String(emailEl.value).toLowerCase().includes(".com"))== false){
+               console.log("success ")
+           }else{
+               console.log("<error effects here>")
+           }
+       })
+       
+   },[])
+   
+   // function for the Sub Button
+   
+   function subscribe(){
+       document.querySelectorAll("input").forEach(el=>el.dispatchEvent(Event))
+       document.querySelectorAll("info").forEach(el=>{
+           el.style.animationName = inform
+           el.style.animationName = inform
+            
+   })
+   }
+
+   //  function subscribe(){
+   //     document.querySelectorAll("input").forEach(el=>el.dispatchEvent(Event))
+   //     document.querySelectorAll("info").forEach(el=>{
+   //         el.style.animationName = "info"
+   //         el.style.animationName =" inform" 
+   //     })
+   //  }
+
+    const EventName ="SUBSCRIBE"
+    const Event = new
+    CustomEvent(EventName,{detail:{event:EventName}})
+
+
+
+
     
-    // function for the Sub Button
+    let count = 30
+
+    function add1() {
+       const resultElement = document.getElementById('timer');
+       if (resultElement) {
+           count = count + 1
+           resultElement.innerText = count;
+       } else {
+           console.error('Element with ID "result" not found.');
+       }
+    }
     
-    function subscribe(){
-        document.querySelectorAll("input").forEach(el=>el.dispatchEvent(Event))
-        document.querySelectorAll("info").forEach(el=>{
-            el.style.animationName = inform
-            el.style.animationName = inform
-             
-    })
+    function add2() {
+       const resultElement = document.getElementById('timer');
+       if (resultElement) {
+           count = count - 1
+           resultElement.innerText = count;
+       } else {
+           console.error('Element with ID "result" not found.');
+       }
     }
 
-    //  function subscribe(){
-    //     document.querySelectorAll("input").forEach(el=>el.dispatchEvent(Event))
-    //     document.querySelectorAll("info").forEach(el=>{
-    //         el.style.animationName = "info"
-    //         el.style.animationName =" inform" 
-    //     })
-    //  }
 
-     const EventName ="SUBSCRIBE"
-     const Event = new
-     CustomEvent(EventName,{detail:{event:EventName}})
+ return (
+   <div className="body">
+     <div className="background">
+         <div className="inner-body">
+           <div className="Card-payment-info">
+               <ul className="nav">
+                   <li>
+                       <Image src="/Images/productlogo.png"
+                       alt="logo"
+                       width={50}
+                       height={10}
+                       className="logo"
 
+                       />
+                   </li>
+                   <li className="scolar">Scolarité</li>
+               </ul>
 
-  return (
-    <div className="body">
-      <div className="background">
-          <div className="inner-body">
-            <div className="Card-payment-info">
-                <ul className="nav">
-                    <li>
-                        <Image src="/Images/productlogo.png"
-                        alt="logo"
-                        width={50}
-                        height={10}
-                        className="logo"
+               <ul className="card">
+                   <li>
+                   <Image src="/Images/creditcard.png"
+                       alt="logo"
+                       width={50}
+                       height={10}
+                       className="credit-cd"
 
-                        />
-                    </li>
-                    <li className="scolar">Scolarité</li>
-                </ul>
+                       />
+                   </li>
+                   <li className="credit-info"> Credit Payment Information</li>
 
-                <ul className="card">
-                    <li>
-                    <Image src="/Images/creditcard.png"
-                        alt="logo"
-                        width={50}
-                        height={10}
-                        className="credit-cd"
-
-                        />
-                    </li>
-                    <li className="credit-info"> Credit Payment Information</li>
-
-                </ul>
+               </ul>
 
 
-               {/* FORMM */}
-                <form method="post">
+              {/* FORMM */}
+               <form method="post">
 
-                <div className="input-headings">
-                    <ul>
-                    <li className="text"> Email</li>
-                        <li> <Image src="/Images/info.png"
-                        alt="logo"
-                        width={50}
-                        height={10}
-                        className="info"
+               <div className="input-headings">
+                   <ul>
+                   <li className="text"> Email</li>
+                       <li> <Image src="/Images/info.png"
+                       alt="logo"
+                       width={50}
+                       height={10}
+                       className="info"
 
-                        /> </li>
-                    </ul>
-                       
-                       <input type="email" id="email" className="input" placeholder="example@gmail.com" required/>
-    
-                 </div>
-
-
-                 
-                   <div className="payment-method">
-                         Payment Method(s)
-                      <ul>
-                          <li> <Image src="/Images/visa.png"
-                        alt="logo"
-                        width={50}
-                        height={10}
-                        className="visa"
-
-                        /> </li>
-                           <li> <Image src="/Images/mastercard.png"
-                        alt="logo"
-                        width={50}
-                        height={10}
-                        className="visa-2"
-
-                        /> </li>
-                     </ul>
+                       /> </li>
+                   </ul>
+                      
+                      <input type="email" id="email" className="input" placeholder="example@gmail.com" required/>
+   
                 </div>
 
 
-                <div className="input-headings">
-                    <ul>
-                    <li className="text" > Cardholder's Name</li>
-                        <li> <Image src="/Images/info.png"
-                        alt="logo"
-                        width={50}
-                        height={10}
-                        className="info"
+                
+                  <div className="payment-method">
+                        Payment Method(s)
+                     <ul>
+                         <li> <Image src="/Images/visa.png"
+                       alt="logo"
+                       width={50}
+                       height={10}
+                       className="visa"
 
-                        /> </li>
+                       /> </li>
+                          <li> <Image src="/Images/mastercard.png"
+                       alt="logo"
+                       width={50}
+                       height={10}
+                       className="visa-2"
+
+                       /> </li>
                     </ul>
-                       
-                       <input type="text" id="holdersname" className="input" placeholder="Enter name on Card"required/>
-    
-                 </div>
-
-
-
-                 <div className="input-headings">
-                    <ul>
-                    <li className="text"> Card Number</li>
-                        <li> <Image src="/Images/info.png"
-                        alt="logo"
-                        width={50}
-                        height={10}
-                        className="info"
-
-                        /> </li>
-                    </ul>
-                       
-                       <input  id="cardnumber" value={cardNmuber} onChange={(e)=>{
-
-                           if(e.target.value.length <= 16){
-                            setCardNumber(e.target.value)
-                        }
-                        }
-                        } 
-                        className="input" type="number"   placeholder="               -  -  -  -          -  -  -  -      
-                               -  -  -  -           -  -  -  -" required/>
-    
-                 </div>
-
-
-                 <div className="date-cvv" >
-                 <div className="input-headings">
-                    <ul>
-                    <li className="text"> Card Expiry Date</li>
-                        <li> <Image src="/Images/info.png"
-                        alt="logo"
-                        width={50}
-                        height={10}
-                        className="info"
-
-                        /> </li>
-                    </ul>
-                       {/* <div  
-                       onClick={()=>{
-                        console.log("clicked")
-                        dateInputRef.current.showPicker()}}
-                        style={{
-                            display:"flex",
-                            alignItems:"center",
-                            justifyContent:"center",
-                            width:"100%",
-                        }}
-                        
-                        className="input-expiry"
-
-                        >
-                            MM/YY
-                        <input type="date" style={{
-                            visibility:"hidden"
-
-
-                        }} ref={dateInputRef}
-                        placeholder="MM/YY"
-                        />
-                       </div> */}
-                       <input type="date" id="cardexpiry" className="input-expiry"   placeholder="MM/YY" required/>
-    
-                 </div>
-
-                 <div className="input-headings">
-                    <ul>
-                    <li className="text"> CVV</li>
-                        <li> <Image src="/Images/info.png"
-                        alt="logo"
-                        width={50}
-                        height={10}
-                        className="info"
-
-                        /> </li>
-                    </ul>
-                       
-                       <input type="number" id="cvv" className="input-cvv" value={cvv} onChange={(e)=>{
-                        if(e.target.value.length <=3){
-                            setCvv(e.target.value)
-                        }
-                       }}   placeholder="       ----      ----     ----  " required/>
-    
-                 </div>
-
-
                </div>
-              
-               <button type="submit" id="Subscribe" className="subscribe"  name="Subscribe" onClick={() =>subscribe()}>Subscribe</button>
- 
-     
 
-                </form>
 
-            </div>
+               <div className="input-headings">
+                   <ul>
+                   <li className="text" > Cardholder's Name</li>
+                       <li> <Image src="/Images/info.png"
+                       alt="logo"
+                       width={50}
+                       height={10}
+                       className="info"
+
+                       /> </li>
+                   </ul>
+                      
+                      <input type="text" id="holdersname" className="input" placeholder="Enter name on Card"required/>
+   
+                </div>
+
+
+
+                <div className="input-headings">
+                   <ul>
+                   <li className="text"> Card Number</li>
+                       <li> <Image src="/Images/info.png"
+                       alt="logo"
+                       width={50}
+                       height={10}
+                       className="info"
+
+                       /> </li>
+                   </ul>
+                      
+                      <input  id="cardnumber" value={cardNmuber} onChange={(e)=>{
+
+                          if(e.target.value.length <= 16){
+                           setCardNumber(e.target.value)
+                       }
+                       }
+                       } 
+                       className="input" type="number"   placeholder="               -  -  -  -          -  -  -  -             -  -  -  -           -  -  -  -" required/>
+   
+                </div>
+
+
+                <div className="date-cvv" >
+                <div className="input-headings">
+                   <ul>
+                   <li className="text"> Card Expiry Date</li>
+                       <li> <Image src="/Images/info.png"
+                       alt="logo"
+                       width={50}
+                       height={10}
+                       className="info"
+
+                       /> </li>
+                   </ul>
+                      {/* <div  
+                      onClick={()=>{
+                       console.log("clicked")
+                       dateInputRef.current.showPicker()}}
+                       style={{
+                           display:"flex",
+                           alignItems:"center",
+                           justifyContent:"center",
+                           width:"100%",
+                       }}
+                       
+                       className="input-expiry"
+
+                       >
+                           MM/YY
+                       <input type="date" style={{
+                           visibility:"hidden"
+                           background:"#F8F9FB"
+
+
+                       }} ref={dateInputRef}
+                       placeholder="MM/YY"
+                       />
+                      </div> */}
+                      <input type="date" id="cardexpiry" className="input-expiry"   placeholder="" required/>
+   
+                </div>
+
+                <div className="input-headings">
+                   <ul>
+                   <li className="text"> CVV</li>
+                       <li> <Image src="/Images/info.png"
+                       alt="logo"
+                       width={50}
+                       height={10}
+                       className="info"
+
+                       /> </li>
+                   </ul>
+                      
+                      <input type="number" id="cvv" className="input-cvv" value={cvv} onChange={(e)=>{
+                       if(e.target.value.length <=3){
+                           setCvv(e.target.value)
+                       }
+                      }}    placeholder="       ----      ----     ----  " required/>
+   
+                </div>
+
+
+              </div>
+             
+              <button type="submit" id="Subscribe" className="subscribe"  name="Subscribe" onClick={() =>subscribe()}>Subscribe</button>
+
+    
+
+               </form>
+
+           </div>
+
+
+
+
+
+
+            
+
+
 
  {/* SECOND HALF */}
             <div className="Payment-summary">
