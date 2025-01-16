e3ea8de4165938a48816a017e623094ec8615663
ofoegbu stephen <stephenchidindu12@gmail.com>
2025-01-16 14:13:28 +0100

    layout done

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,24 +1,19 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import { RiArrowDropRightLine } from "react-icons/ri";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowDown, MdMenu, MdClose } from "react-icons/md";
 import { LuSearch } from "react-icons/lu";
-
-// Sample components for each menu item
-
-const DashboardContent = () => <div>Dashboard Content</div>;
-const AnalyticContent = () => <div>Analytic Content</div>;
-const MarketingContent = () => <div>Marketing Material Content</div>;
-const WithdrawContent = () => <div>Withdraw Earnings Content</div>;
-const TransactionContent = () => <div>Transaction History Content</div>;
-const SettingsContent = () => <div>Settings Content</div>;
+import DashboardContent from "../dashboard/DashboardContent/page";
+import AnalyticContent from "../dashboard/AnalyticContent/page";
+import MarketingContent from "../dashboard/MarketingContent/page";
+import WithdrawContent from "../dashboard/WithdrawContent/page";
+import TransactionContent from "../dashboard/TransactionContent/page";
+import SettingsContent from "../dashboard/SettingsContent/page";
 
 export default function Page() {
-  // State to manage the selected menu
-  
   const [selectedMenu, setSelectedMenu] = useState("Dashboard");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  // Render the corresponding component based on the selected menu
   const renderContent = () => {
     switch (selectedMenu) {
       case "Dashboard":
@@ -38,20 +33,38 @@ export default function Page() {
     }
   };
 
+  const handleMenuClick = (menuName) => {
+    setSelectedMenu(menuName);
+    setIsSidebarOpen(false); // Close the sidebar on menu click
+  };
+
   return (
-    <div className="flex h-screen overflow-y-hidden">
+    <div className="flex flex-row h-screen">
       {/* Sidebar */}
-      <div className="w-[27rem] bg-white text-black p-4">
-        <div className="flex gap-2 items-center md:pl-8">
+      <div
+        className={`w-full md:w-[20rem] bg-white text-black p-4 md:h-full transition-transform duration-700 ease-in-out ${
+          isSidebarOpen
+            ? "transform translate-x-0"
+            : "transform -translate-x-full md:translate-x-0 "
+        } absolute left-0 z-10 md:relative md:translate-x-0`}
+      >
+        <div className="flex gap-2 items-center pl-4">
           <img src="/image/logo.png" alt="logo" className="w-10" />
-          <h2 className="text-2xl py-6 font-bold font-roboto">SCOLARITÉ</h2>
+          <h2 className="text-2xl py-10 font-bold font-roboto flex-1">
+            SCOLARITÉ
+          </h2>
+          {/* Close button for mobile */}
+          <MdClose
+            className="block md:hidden text-2xl cursor-pointer"
+            onClick={() => setIsSidebarOpen(false)}
+          />
         </div>
         <div
           className="flex flex-col justify-between"
-          style={{ height: "calc(100vh - 100px)" }}
+          style={{ height: "calc(100vh - 120px)" }}
         >
           {/* Dashboard menu */}
-          <div className="mt-10">
+          <div className="mt-6 space-y-2">
             {[
               { name: "Dashboard", icon: "/image/dashboard.svg" },
               { name: "Analytic", icon: "/image/analytic.svg" },
@@ -62,27 +75,38 @@ export default function Page() {
             ].map((menu) => (
               <div
                 key={menu.name}
-                onClick={() => setSelectedMenu(menu.name)}
-                className={`flex items-center gap-2 p-2 pl-8 cursor-pointer rounded-md ${
-                  selectedMenu === menu.name
-                    ? "bg-black text-white"
-                    : "hover:bg-black hover:text-white"
-                } group`}
+                onClick={() => handleMenuClick(menu.name)}
+                className={`flex items-center justify-start gap-2 p-2 pl-4 cursor-pointer rounded-md ${
+                  selectedMenu === menu.name ? "bg-black text-white" : ""
+                }`}
               >
-                <img
-                  src={menu.icon}
-                  alt={`${menu.name}_icon`}
-                  className="w-8 group-hover:filter group-hover:brightness-0 group-hover:invert"
+                <div className="flex items-center gap-2 flex-1">
+                  <img
+                    src={menu.icon}
+                    alt={`${menu.name}_icon`}
+                    className="w-8"
+                    style={{
+                      filter:
+                        selectedMenu === menu.name
+                          ? "brightness(0) invert(1)"
+                          : "none",
+                    }}
+                  />
+                  <p className="font-roboto font-medium">{menu.name}</p>
+                </div>
+                <RiArrowDropRightLine
+                  size={30}
+                  className={`${
+                    selectedMenu === menu.name ? "text-white" : "text-black"
+                  }`}
                 />
-                <p className="font-roboto font-medium">{menu.name}</p>
-                <RiArrowDropRightLine size={30} />
               </div>
             ))}
           </div>
 
           {/* Profile menu */}
-          <div>
-            <div className="flex items-center flex-row gap-2 p-2 cursor-pointer rounded-md">
+          <div className="mt-8">
+            <div className="flex items-center gap-2 p-2 pl-4 cursor-pointer rounded-md">
               <img
                 src="/image/profile.svg"
                 alt="dashboard_profile"
@@ -100,22 +124,29 @@ export default function Page() {
         </div>
       </div>
 
-      {/* Main Content */}
-      <div className="w-full bg-gray-100">
+      {/* Main content */}
+      <div
+        className={`flex-1 bg-gray-100 ${
+          isSidebarOpen ? "overflow-hidden" : ""
+        }`}
+      >
         {/* Header menu */}
-        <div className="flex items-center justify-between p-10">
-          <div>
-            <h2 className="font-roboto font-medium text-[20px]">
+        <div className="flex flex-row items-start md:items-center justify-between p-6 gap-4">
+          <div className="flex items-center justify-between w-full md:mt-0 mt-2">
+            <MdMenu
+              className="block md:hidden text-2xl cursor-pointer"
+              onClick={() => setIsSidebarOpen(true)}
+            />
+            <h2 className="font-roboto font-medium md:text-[20px] text-[15px]">
               Hello Okechukwu,
             </h2>
           </div>
-          {/* Dashboard search */}
-          <div className="flex items-center gap-5">
-            <div className="relative">
+          <div className="flex items-center gap-4 w-full md:w-auto">
+            <div className="relative w-full md:w-60">
               <LuSearch className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500 cursor-pointer" />
               <input
                 type="text"
-                className="bg-white outline-none w-60 pl-10 p-2 rounded-md"
+                className="bg-white outline-none w-full pl-10 p-2 rounded-md"
                 placeholder="Search..."
               />
             </div>
@@ -123,7 +154,7 @@ export default function Page() {
         </div>
 
         {/* Dynamic content area */}
-        <div className="p-10">{renderContent()}</div>
+        <div className="p-6">{renderContent()}</div>
       </div>
     </div>
   );
